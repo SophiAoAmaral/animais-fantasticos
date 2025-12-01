f954215c86cb52b20394063dd9debf883e5f3264
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2025-12-01 20:31:05 -0300

    class

--- classes-e-atributos/script copy.js
+++ classes-e-atributos/script copy.js
@@ -1,14 +1,25 @@
-const imgs = document.querySelectorAll('img');
+const menu = document.querySelector('.menu');
 
-imgs.forEach((img, index, array)=>{
-    console.log(img, index, array)
-});
+console.log(menu.classList)//lista de classes
+menu.classList.add('ativo');
+menu.classList.remove('azul');
+//menu.classList.toggle('azul'); adiciona se n tiver e se tiver tira
 
-//transformar coisas q nao sao array em array
+//verifica se possui a classe
+if (menu.classList.contains('azul')) {
+  menu.classList.add('possui azul')
+}
 
-const titulos = document.getElementsByClassName('titulo');
-const titulosArray = Array.from(titulos);
+//className retorna uma string
 
-titulosArray.forEach((titulo)=>{
-    console.log(titulo)
-})
+menu.className += ' remove';
+
+const animais = document.querySelector('.animais');
+
+console.log(animais.attributes);
+
+const img = document.querySelector('img');
+
+console.log(img.getAttribute('src'));
+
+img.setAttribute('alt' , 'e uma raposa ')
--- classes-e-atributos/script.js
+++ classes-e-atributos/script.js
@@ -1,23 +1,31 @@
-// Mostre no console cada parágrado do site
-const paragrafos = document.querySelectorAll('p');
+// Adicione a classe ativo a todos os itens do menu
 
-paragrafos.forEach((p) =>{
-    console.log(p.innerText)
+const itensMenu = document.querySelectorAll('.menu a');
+
+itensMenu.forEach((item)=>{
+  item.classList.add('ativo')
 })
-// Mostre o texto dos parágrafos no console
 
-
-// Como corrigir os erros abaixo:
-const imgs = document.querySelectorAll('img');
-
-imgs.forEach((img, index) => {
-  console.log(img, index);
+// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+itensMenu.forEach((item)=>{
+  item.classList.remove('ativo')
 });
 
-let i = 0;
-imgs.forEach(() => 
-  console.log(i++)
-);
+itensMenu[0].classList.add('ativo')
 
-imgs.forEach(() => i++);
+// Verifique se as imagens possuem o atributo alt
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((img) =>{
+  const possuiAlt = img.hasAttribute('alt');
+  console.log(img, possuiAlt)
+})
+
+
+
+// Modifique o href do link externo no menu
+
+const link = document.querySelector('a[href^="http"]');
+
+link.setAttribute('href', 'aaa')
 
