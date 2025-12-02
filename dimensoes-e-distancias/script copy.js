3af403172163f6990659772afc680a426f5bf08a
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2025-12-01 21:17:06 -0300

    dimensoes e distancias

---
+++ dimensoes-e-distancias/script copy.js
@@ -0,0 +1,6 @@
+const listaAnimais = document.querySelector('.animais-lista');
+
+const height = listaAnimais.scrollHeight;
+
+const animaisTop = listaAnimais.offsetTop;
+
---
+++ dimensoes-e-distancias/script.js
@@ -0,0 +1,49 @@
+// Verifique a distância da primeira imagem
+// em relação ao topo da página
+
+const img = document.querySelector('img');
+const imgTop = img.offsetTop;
+console.log(imgTop)
+
+// Retorne a soma da largura de todas as imagens
+function somaImagens (){
+const imagens = document.querySelectorAll('img');
+let soma = 0 ;
+    imagens.forEach((imagem)=>{
+    soma += imagem.offsetWidth;
+
+    console.log(soma)
+    
+})};
+
+window.onload = function(){
+  somaImagens();
+}
+
+// Verifique se os links da página possuem
+// o mínimo recomendado para telas utilizadas
+// com o dedo. (48px/48px de acordo com o google)
+
+const links = document.querySelectorAll('a');
+
+links.forEach((link)=>{
+    const linkWidth = link.offsetWidth
+    const linkHeight = link.offsetHeight
+
+    if(linkHeight >= 48 && linkWidth >= 48){
+        console.log(link, 'possui acessibilidade')
+    }else{
+        console.log(link, 'nao possui acessibilidade')
+    }
+
+})
+
+// Se o browser for menor que 720px,
+// adicione a classe menu-mobile ao menu
+
+const browseSmall = window.matchMedia('(max-width:720px)').matches;
+
+if(browseSmall){
+    const menu = document.querySelector('.menu')
+    menu.classList.add('menu-mobile')
+}
