61e4f44f6f6508289566f2dc3d4e7abfe4732470
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2025-12-04 16:18:39 -0300

    animaco scrolll

---
+++ animacao-ao-scroll/script.js
@@ -0,0 +1,96 @@
+function initTab(){
+
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+if(tabMenu.length && tabContent.length){
+    tabContent[0].classList.add('ativo');
+
+function activeTab(index) {
+  tabContent.forEach((content) => {
+    content.classList.remove('ativo');
+  });
+  tabContent[index].classList.add('ativo');
+}
+
+
+
+tabMenu.forEach((itemMenu, index) => {
+  itemMenu.addEventListener('click', () => {
+    activeTab(index);
+  });
+});
+}
+}
+initTab();
+
+
+
+
+function initAccordion(){
+const accordionList = document.querySelectorAll('.js-accordion dt');
+accordionList[0].classList.add('ativo');
+accordionList[0].nextElementSibling.classList.add('ativo');
+
+
+if (accordionList.length){
+function activeAccordion(){
+  
+  this.classList.toggle('ativo');
+  this.nextElementSibling.classList.toggle('ativo');
+}
+accordionList.forEach((item)=>{
+  item.addEventListener('click', activeAccordion);
+})
+}
+}
+initAccordion()
+
+
+
+function initScrollSuave(){
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+function scrollToSection(e){
+  e.preventDefault();
+  const href = e.currentTarget.getAttribute('href');
+  const section = document.querySelector(href);
+
+const topo = section.offsetTop;
+  window.scrollTo({
+    top: topo,
+   behavior:'smooth'
+})
+}
+
+
+linksInternos.forEach((link) =>{
+  link.addEventListener('click', scrollToSection);
+})
+}
+
+initScrollSuave();
+ 
+
+function initAnimacaoScroll(){
+
+const sections = document.querySelectorAll('.js-scroll');
+const windowMetade = window.innerHeight * 0.6
+
+
+function animaScroll(){
+  sections.forEach((section)=>{
+    const sectionTop = section.getBoundingClientRect().top;
+    const isSectionVisible = (sectionTop - windowMetade)< 0;
+    if(isSectionVisible){
+      section.classList.add('ativo');
+    }
+  })
+}
+
+window.addEventListener('scroll', animaScroll)
+
+
+}
+
+initAnimacaoScroll()
