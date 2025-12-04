ca0515df5b04a660d11801b55d457eebf5a0a900
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2025-12-04 15:15:17 -0300

    scroll suave ate uma section

--- scroll-suave-por-link/script.js
+++ scroll-suave-por-link/script.js
@@ -40,3 +40,27 @@ accordionList.forEach((item)=>{
 })
 }
 }
+
+function initScrollSuave(){
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+function scrollToSection(e){
+  e.preventDefault();
+  const href = e.currentTarget.getAttribute('href');
+  const section = document.querySelector(href);
+
+const topo = section.offsetTop;
+  window.scrollTo({
+    top: topo,
+   behavior:'smooth'
+})
+}
+
+
+linksInternos.forEach((link) =>{
+  link.addEventListener('click', scrollToSection);
+})
+
+
+}
+
